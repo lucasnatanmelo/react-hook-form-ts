d5be650ffaacb41b4accdfdcdab08cf7718b1666
Lucas Almeida <lucas.almeida@fpf.br>
2022-08-08 00:03:34 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { useForm } from 'react-hook-form';
+import Headers from './Header';
+import './styles.css';
 
-function App() {
+let renderCount = 0;
+
+type FormValues = {
+  firstName: string;
+  lastName: string;
+  age: number;
+  gender: string;
+  developer: string;
+};
+
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+  // eslint-disable-next-line no-plusplus
+  renderCount++;
+  const onSubmit = (data: FormValues) => {
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Headers
+        renderCount={renderCount}
+        description="Performant, flexible and extensible forms with easy-to-use validation."
+      />
+      <label htmlFor="firstName">First Name:</label>
+      <input
+        {...register('firstName', { required: 'This is required.' })}
+        id="firstName"
+      />
+      {errors.firstName && <p>{errors.firstName.message}</p>}
+
+      <label htmlFor="lastName">Last Name:</label>
+      <input {...register('lastName', { required: true, minLength: 5 })} />
+
+      <label htmlFor="age">Age</label>
+      <input
+        type="number"
+        {...register('age', { valueAsNumber: true })}
+        id="age"
+      />
+
+      <label htmlFor="gender" />
+      <select {...register('gender')} id="gender">
+        <option value="">Select...</option>
+        <option value="male">male</option>
+        <option value="female">female</option>
+      </select>
+
+      <label htmlFor="developer">Are you a developer?</label>
+      <input {...register('developer')} value="yes" type="checkbox" />
+
+      <input type="submit" />
+    </form>
   );
 }
-
-export default App;
